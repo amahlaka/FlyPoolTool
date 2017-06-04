3a3226a172a3d82ac0a697d65dbf39d219f47cdc
Arttu <arttu@MacBookAir.local>
2017-06-04 12:55:08 +0300

    Made it look prettier and fancy, also added visible update timer

--- tool.py
+++ tool.py
@@ -11,15 +11,43 @@ def getStats(user):
     output = response.json()
     return(output)
 
+
+def clearScreen():
+    """Check os and clear the screen."""
+    import platform
+    import os
+    osystem = platform.system()
+
+    if(osystem == "Windows"):
+        os.system('cls')
+    if(osystem == "Linux" or osystem == "Darwin" ):
+        os.system('clear')
+    return
+def updateDelay():
+    times = 10
+    while (times > 0):
+        print('.', sep=' ', end='', flush=True)
+        time.sleep(1)
+        times = times - 1
+    print(" Updating")
+    return
+
+
 while(True):
     stats = getStats(wallet)
     hashrate = stats["hashRate"]
     payout = stats["payouts"]
+
     unpaid = str(stats["unpaid"]).rjust(9, '0')
-    print(payout[1]["paidOn"])
-    print(hashrate)
-    print(str(stats["avgHashrate"])+'H/s')
+    paid = str(payout[1]["amount"]).rjust(9, '0')
+    paidf = paid[:-8]+'.'+paid[-8:]
+    clearScreen()
+    print('------------------------------------------------------------')
+    print("Latest Payout: "+payout[1]["paidOn"]+" Amount: "+paidf)
+    print("Reported Hashrate: "+hashrate)
+    print("Effective Hashrate: "+str(stats["avgHashrate"])+'H/s')
     fxd = unpaid[:-8]+'.'+unpaid[-8:]
 
-    print(fxd)
-    time.sleep(10)
+    print("Unpaid: "+fxd)
+    print('------------------------------------------------------------')
+    updateDelay()
